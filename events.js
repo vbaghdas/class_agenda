696734abc54421af4397066470c49ac01f3db928
Carlos Blandino <cblandino@msn.com>
2017-09-19 11:47:38 -0700

    updated code for event_data, event.html, and event.js to enable template

--- events.js
+++ events.js
@@ -17,42 +17,40 @@ function logCurrentEvent(eventlist){
     console.log(eventlist);
 }
 
+
 function Display(eventList){
-    var arr = $(".avatarContainer");
-    console.log(arr);
-    for(var i = 0; i < eventList.length; ++i){
-        console.log(eventList[i].avatar);
-        $(arr[i]).append($("<img class='avatar'>").attr("src",eventList[i].avatar));
-    }
+    var dataListObject = {};
+     for(var i = 0; i < eventList.length; ++i) {
+         dataListObject['avatar'+i] = eventList[i].avatar;
+         dataListObject['logo'+i] = eventList[i].logo;
+         dataListObject['date'+i] = eventList[i].date;
+         dataListObject['name'+i] = eventList[i].name;
+         dataListObject['facility'+i] = eventList[i].facility;
+         dataListObject['location'+i] = eventList[i].location;
+
+     }
+
+
+    var elements = $("body *:not(script)");
+    elements.each(function(){
+            var innerText = $(this)[0].innerText;
+
+            var searchRegex = /{(.*)}/g;
+            //if match is found assign it to matchesArray
+            var matchesArray = innerText.match(searchRegex);
+            //check for a match
+
+            if(matchesArray!==null && matchesArray.length === 1) {
+                //when match is found strip off curly braces and assign to entry
+                var entry = matchesArray[0].slice(1, -1);
+                //check eventList id an entry exist and if so the current elements html is fed the eventList item
+                if (dataListObject[entry]) {
+                    $(this).html(dataListObject[entry]);
+                }
+            }
+    });
+
 }
 
-// function foo() {
-//     $(document).ready(init);
-//     function init() {
-//         findTags(dataObject);
-//     };
-//     var dataObject = {
-//         avatar: "<img src='https://pbs.twimg.com/profile_images/465918286257868800/61etP8rK.jpeg'>",
-//         event: 'Marv Chan comes to LFZ',
-//         date: 'Meet Marv and the LFZ team, 3 people employed at KBB'
-//     };
-//     function findTags(fields){
-//         var elements = $("body *:not(script):not(img):not(iframe)");
-//         elements.each(function(){
-//             var innerText = $(this)[0].innerText;
-//             var searchRegex = /{(.*)}/g;
-//             var matchesArray = innerText.match(searchRegex);
-//             if(matchesArray.length===1){
-//                 var entry = matchesArray[0].slice(1,-1);
-//                 if(fields[entry]){
-//                     $(this).html(fields[entry]);
-//                 }
-//             }
-//         });
-//     }
-// }
 
 
-$('document').ready(function(){
-
-});
