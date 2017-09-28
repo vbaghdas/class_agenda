c8281cdbeca1102a137d2ef48d356027d459f5ae
vbaghdas <vbaghdas@gmail.com>
2017-09-28 16:21:56 -0700

    started calendar day functionality

--- client/src/components/calendar/index.js
+++ client/src/components/calendar/index.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
+import Modal from './modal';
+import CalDay from './calDay';
 
 
 class Calendar extends Component{
     constructor(props){
         super(props);
-        var now = new Date();
-        this.currentYear = now.getFullYear();
-        this.currentMonth = now.getMonth();
-        this.currentDate = now.getDate();
-        this.currentDay = now.getDay();
+        var date = new Date();
+        this.currentYear = date.getFullYear();
+        this.currentMonth = date.getMonth();
+        this.currentDate = date.getDate();
+        this.currentDay = date.getDay();
+        this.now = new Date();
+        this.state = { isOpen: false };
     }
+
+    toggleModal = () => {
+        this.setState({
+              isOpen: !this.state.isOpen
+        });
+    };
 
     createRows(){
         var date = new Date();
@@ -22,12 +32,22 @@ class Calendar extends Component{
         var dayArr = [];
 
         for(let i = 0; i < weeklength * rowsCount; ++i){
-            dayArr.push(startDate.getDate());
+            dayArr.push({date:startDate.getDate(),month:9});
             startDate.setDate(startDate.getDate()+1);
         }
 
         dayArr = dayArr.map((item,index)=>{
-            return <td className="calDay" key={index}>{item}</td>
+            let targetEvent = null;
+            let events = this.props.events;
+            for(let i = 0; i < events.length; ++i){
+                console.log("event month",events[i].formattedDate.getMonth() );
+                console.log("calDay month",item.month);
+                if(events[i].formattedDate.getMonth() === item.month && events[i].formattedDate.getDate() === item.date){
+                    targetEvent = events[i];
+                    console.log("targetEvent",targetEvent);
+                }
+            }
+            return <CalDay className="calDay" key={index}  onClick={this.toggleModal} event={targetEvent}>{item.date}</CalDay>
         });
 
         var rowArr = [];
@@ -54,48 +74,12 @@ class Calendar extends Component{
                         {this.createRows()}
                     </tbody>
                 </table>
+                <Modal show={this.state.isOpen}
+                       onClose={this.toggleModal}>
+                </Modal>
             </div>
         )
     }
 }
 
 export default Calendar;
-
-
-var dayLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-var monthLabels = [
-    'January', 'February', 'March', 'April', 'May',
-    'June', 'July', 'August', 'September',
-    'October', 'November', 'December'
-];
-
-var currentDate = new Date();
-var currentDay = currentDate.getDate();
-var currentMonth = currentDate.getMonth();
-var currentYear = currentDate.getFullYear();
-
-function daysInMonth(year, month) {
-    return (new Date(year, ++month, 0)).getDate();
-}
-
-function createCalendar(month, year) {
-    this.month = (isNaN(month) || month === null) ? currentMonth : month;
-    this.year  = (isNaN(year) || year === null) ? currentYear : year;
-}
-
-function generateCalendar(){
-        var firstDay = new Date(this.year, this.month, 1);
-        var startingDay = firstDay.getDay();
-        var monthLength = daysInMonth(this.year, this.month);
-        var monthName = monthLabels[this.month];
-
-        for(var i = 0; i <= 6; i++ ){
-            var calendarHeaderDay = $('<td class="calHeaderDay">');
-            calendarHeaderDay.text(dayLabels[i]);
-            calendarHeader.append(calendarHeaderDay);
-        }
-
-        var day = 1;
-        var calendarDayRow = '<tr class="calDayRow">';
-};
