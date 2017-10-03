349db898a99d7469161dc0fa6c5680a40ec2ba27
JEN-HAO TAN <tan0929@outlook.com>
2017-10-02 17:20:35 -0700

    remove pseudo class 'hover' from css - James

--- client/src/components/calendar/calDay.js
+++ client/src/components/calendar/calDay.js
@@ -3,13 +3,28 @@ import React, { Component } from 'react';
 class CalDay extends Component {
     constructor(props){
         super(props);
+        this.state = {ishover:false};
+        this.onHover = this.onHover.bind(this);
+        this.onOut = this.onOut.bind(this);
+    }
+
+
+    onHover(){
+        this.setState({ishover:true});
+    }
+
+    onOut(){
+        this.setState({ishover:false});
     }
 
     render(){
         let event = this.props.event;
         if(event){
             return (
-                <td className="calDay" onClick={()=>this.props.onClick(event)}><img className="avatar" src={event.avatar}/></td>
+                <td className={this.state.ishover? "calDay enlarge": "calDay"}
+                onMouseEnter={()=>this.onHover()} 
+                onMouseLeave={()=>this.onOut()}
+                onClick={()=>this.props.onClick(event)}><img className="avatar" src={event.avatar}/></td>
             )
         }else{
             return (
