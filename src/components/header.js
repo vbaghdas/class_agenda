aeaae85e8fc8564cdb1bf88e4a1bd552bf32e0a5
vbaghdas <vbaghdas@gmail.com>
2017-11-07 22:18:57 -0800

    fixed order of icons in navbar to match carousel

--- src/components/header.js
+++ src/components/header.js
@@ -11,8 +11,8 @@ export default () => (
         <Link to="/" className="brand-logo"><img src={logo}/></Link>
         <ul className="right hide-on-med-and-down">
             <li><NavLink exact to="/"><i className="fa fa-home fa-2x"></i></NavLink></li>
-            <li><NavLink to="/calendar"><i className="fa fa-calendar fa-2x"></i></NavLink></li>
             <li><NavLink to="/events"><i className="fa fa-list-ul fa-2x"></i></NavLink></li>
+            <li><NavLink to="/calendar"><i className="fa fa-calendar fa-2x"></i></NavLink></li>
             <li><NavLink to="/gamepad"><i className="fa fa-gamepad fa-2x"></i></NavLink></li>
             <li><NavLink to="/about"><i className="fa fa-users fa-2x"></i></NavLink></li>
         </ul>
